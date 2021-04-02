ba6557efcd49e5acb0bcf99d0357b467d772bae8
Altela Eleviansyah Pramardhika <68892527+Altela@users.noreply.github.com>
2021-04-02 22:37:26 +0800

    Delete printing_partner.py

--- printing_partner.py
+++
@@ -1,4 +0,0 @@
-# printing partner
-models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-partners = models.execute_kw(db, uid, password, 'res.partner', 'search', [[]])
-print(partners)
