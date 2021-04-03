12d7546dc510b0687c6dd5b050e5d8146511bfa0
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2021-04-03 14:55:13 +0800

    Update SEARCH product by ID and READ that Product

--- list_product.py
+++ list_product.py
@@ -1,4 +1,27 @@
+# Search Product
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-product = models.execute_kw(db, uid, password, 'product.template', 'search',[[
-]])
-print(product)
+searchByID = models.execute_kw(db, uid, password,
+    'product.product', 'search',
+    [[['id', '=', yourDesiredID]]], #Put Your Product ID here
+    # {'limit': 1})
+
+# Reading Product from table product.product
+read = models.execute_kw(db, uid, password,'product.product', 'read',
+        [searchByID], {'fields': 
+        ['name', #Product Name
+        'type', #Product Type
+        'sale_ok', #Can Be Sold (Boolean checkbox status)
+        'purchase_ok', #Can Be Purchased (Boolean checkbox status)
+        'can_be_expensed', #Can Be Expensed (Boolean checkbox status)
+        'categ_id', #Product Category
+        'default_code', #Print Internal Reference
+        'barcode', #Product Barcode
+        'list_price', #Sales Price
+        'standard_price', #Cost
+        'uom_id', #Unit of Measure ID
+        'uom_po_id', #Purchase Unit of Measure ID
+        'description', #Internal Notes
+        'available_in_pos',
+]})
+
+print(read)
