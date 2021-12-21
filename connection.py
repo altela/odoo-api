e364fde6bf1c72215dbfb3b3f36717babae34415
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2021-12-21 20:27:01 +0800

    Cleaning up codes, moving endpoint to endpoint.py & establish to establish.py

--- connection.py
+++ connection.py
@@ -1,21 +1,13 @@
 import xmlrpc.client
 
 # Establishing Connection
-url = 'http://yourDomain-or-yourIP'
+url = 'https://yourDomain-or-yourIP'
 db = 'yourDatabaseName'
 username = 'yourOdooInternalUser'
 password = 'yourPassword'
 
+# Instructions
 # Fill url with your domain or your IP.
 # Fill db with your database name
 # Fill username with your odoo internal user. Make sure that this username has authotrity needed to do certain job like creating quotation, cancelling invoice, etc.
 # Fill password with password
-
-# Logging in Connection
-<<<<<<< HEAD
-=======
-import xmlrpc.client
->>>>>>> 5f330c4c2c3bac2e96e9338925979f69fd5de120
-models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, password, {})
