96f89b43bc31d4f2317e2c01129f4a8d6b5bc2ef
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-09-22 10:24:11 +0800

    Update attendance_create.py

--- attendance_create.py
+++ attendance_create.py
@@ -1,5 +1,9 @@
+import xmlrpc.client
+from endpoint import models
+from establish import *
+
 new_attendance = models.execute_kw(db, uid, password, 'hr.attendance', 'create', [{
-    'employee_id': 1, # put the employee ID (integer)
+    'employee_id': 1, # put the employee ID (integer), you can first fetch the employee ID and put it here (but that's on different file)
     'check_in': datetime.strftime("%Y-%m-%d %H:%M:%S"), # put the date as string
   }]
 )
